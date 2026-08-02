47f1ec677eb9d07295a1619b562c81ac19aa14a6
ahmed yasser <ahmedya3ser8@gmail.com>
2026-08-02 17:30:24 +0300

    feat: fix routes on server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,27 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'products/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'categories/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'brands/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'cart/checkout/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'cart/checkout/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  },
 ];
